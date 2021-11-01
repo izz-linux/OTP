9ffef39e8ef3afe9e9a7707e7c544cdaf661bb2d
Izz Noland <izz@shipt.com>
2021-11-01 04:14:56 -0500

    initial googleAuth implementation for server-side as well as client-side

---
+++ go.mod
@@ -0,0 +1,2 @@
+module github.com/izz-linux/OTP
+
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// lastpass
+	secret := "<put_secret_here>"
+	showOTP("lastpass", secret)
+
+	// dropbox
+	secret = "<put_secret_here"
+	showOTP("dropbox", secret)
+}
